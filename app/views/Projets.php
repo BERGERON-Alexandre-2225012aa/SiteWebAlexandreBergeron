0562f7c7ec660f42114fb4c330cdc286af613ae5
GruZbob <alexandre.bergeron@etu.univ-amu.fr>
2024-01-25 01:08:59 +0100

    update projets

--- app/views/Projets.php
+++ app/views/Projets.php
@@ -12,32 +12,36 @@ class Projets
 
         <div class="mainDiv">
             <div class="backgroundDiv projectDiv">
-                <label class="projectTitle"><u>KAMA Séisme</u></label>
+                <label class="projectTitle"><u>Spartiates Hockey Game</u></label>
+                <label class="projectDate">2024</label>
                 <div class="projectContentDiv">
                     <div class="projectTextDiv">
                         <label class="projectSubtitle">Présentation</label>
                         <label class="projectDescriptionText">
-                            Il s'agit d'une application développée en <label class="importantWord">Java</label> à l'aide de la bibliothèque <label class="importantWord">JavaFX</label>.
-                            Elle permet l'utilisation de fichiers au format <label class="importantWord">.csv</label> contenant des données liées à des séismes enregistrés sur le site <a href="https://www.sisfrance.net/" class="importantWord">SisFrance.net</a>.
-                            <br> <br> Projet réalisé dans un cadre scolaire en collaboration avec ALI Mdallah, BIHI Ayoub, et FERRIER Killian.
-                            <br> <br> Accessible via GitHub <a class="importantWord" href="https://github.com/BERGERON-Alexandre-2225012aa/S201_ProjetSeisme">ici</a>.
+                            Il s'agit d'un jeu développé pour <label class="importantWord">l'équipe de Hockey sur Glace de Marseille</label>. Accessible depuis le <label class="importantWord">Web</label>, ce projet a été développé en <label class="importantWord">PHP</label>, <label class="importantWord">HTML</label>, <label class="importantWord">CSS</label> et <label class="importantWord">JavaScript</label>.
+                            <br>Le jeu a pour but de faire connaître aux joueurs les règles du Hockey sur Glace tout en permettant aux organisateurs d'animer les tiers-temps pendant les matchs.
+                            <br>Le jeu se présente sous la forme d'un <label class="importantWord">quiz interactif et animé</label>.
+                            Une interface administrateur est également disponible afin de permettre aux organisateurs de gérer les questions du quiz stockées dans la <label class="importantWord">base de données</label> du site.
+                            <br> <br> Projet réalisé dans un cadre scolaire en collaboration avec BIHI Ayoub, BOUT Thomas, HOSPITAL Jules et LOUARN Mathis en utilisant la <label class="importantWord">méthode Agile</label>.
+                            <br> <br> Site en ligne accessible <a class="importantWord" href="https://spartiatejeu.alwaysdata.net/">ici</a>.
                         </label>
                     </div>
                     <div class="projectTextDiv">
                         <label class="projectSubtitle">Fonctionnalités</label>
                         <div class="projectFunctionalitiesDiv">
-                            <label class="projectDescriptionText">• Naviguer à travers 3 pages différentes depuis un menu</label>
-                            <label class="projectDescriptionText">• Sélectionner un fichier <label class="importantWord">.csv</label></label>
-                            <label class="projectDescriptionText">• Choisir une période à analyser</label>
-                            <label class="projectDescriptionText">• Accéder à des statistiques</label>
-                            <label class="projectDescriptionText">• Accéder à une carte intéractive</label>
+                            <label class="projectDescriptionText">• Quiz interactif et animé avec des pixel-arts</label>
+                            <label class="projectDescriptionText">• Page entièrement dédiée aux règles du Hockey sur Glace</label>
+                            <label class="projectDescriptionText">• Login administrateur</label>
+                            <label class="projectDescriptionText">• Interface administrateur permettant la gestion du quiz</label>
+                            <label class="projectDescriptionText">• Administrateur : ajouter / modifier / supprimer de questions</label>
+                            <label class="projectDescriptionText">• Administrateur : créer une room, lancer et stopper le jeu</label>
                         </div>
                     </div>
                 </div>
                 <div class="projectImageDiv">
-                    <img class="projectImage reveal" src="/assets/ressources/projets/kamaseisme.png">
-                    <img class="projectImage reveal" src="/assets/ressources/projets/kamaseisme_stats.png">
-                    <img class="projectImage reveal" src="/assets/ressources/projets/kamaseisme_map.png">
+                    <img class="projectImage spartiatesProjectImage reveal" src="/assets/ressources/projets/spartiates_accueil.png">
+                    <img class="projectImage spartiatesProjectImage reveal" src="/assets/ressources/projets/spartiates_quiz.png">
+                    <img class="projectImage spartiatesProjectImage reveal" src="/assets/ressources/projets/spartiates_admin.png">
                 </div>
             </div>
         </div>
@@ -45,6 +49,7 @@ class Projets
         <div class="mainDiv">
             <div class="backgroundDiv projectDiv">
                 <label class="projectTitle"><u>Croustagram</u></label>
+                <label class="projectDate">2023</label>
                 <div class="projectContentDiv">
                     <div class="projectTextDiv">
                         <label class="projectSubtitle">Présentation</label>
@@ -78,7 +83,41 @@ class Projets
 
         <div class="mainDiv">
             <div class="backgroundDiv projectDiv">
+                <label class="projectTitle"><u>KAMA Séisme</u></label>
+                <label class="projectDate">2023</label>
+                <div class="projectContentDiv">
+                    <div class="projectTextDiv">
+                        <label class="projectSubtitle">Présentation</label>
+                        <label class="projectDescriptionText">
+                            Il s'agit d'une application développée en <label class="importantWord">Java</label> à l'aide de la bibliothèque <label class="importantWord">JavaFX</label>.
+                            Elle permet l'utilisation de fichiers au format <label class="importantWord">.csv</label> contenant des données liées à des séismes enregistrés sur le site <a href="https://www.sisfrance.net/" class="importantWord">SisFrance.net</a>.
+                            <br> <br> Projet réalisé dans un cadre scolaire en collaboration avec ALI Mdallah, BIHI Ayoub, et FERRIER Killian.
+                            <br> <br> Accessible via GitHub <a class="importantWord" href="https://github.com/BERGERON-Alexandre-2225012aa/S201_ProjetSeisme">ici</a>.
+                        </label>
+                    </div>
+                    <div class="projectTextDiv">
+                        <label class="projectSubtitle">Fonctionnalités</label>
+                        <div class="projectFunctionalitiesDiv">
+                            <label class="projectDescriptionText">• Naviguer à travers 3 pages différentes depuis un menu</label>
+                            <label class="projectDescriptionText">• Sélectionner un fichier <label class="importantWord">.csv</label></label>
+                            <label class="projectDescriptionText">• Choisir une période à analyser</label>
+                            <label class="projectDescriptionText">• Accéder à des statistiques</label>
+                            <label class="projectDescriptionText">• Accéder à une carte intéractive</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="projectImageDiv">
+                    <img class="projectImage reveal" src="/assets/ressources/projets/kamaseisme.png">
+                    <img class="projectImage reveal" src="/assets/ressources/projets/kamaseisme_stats.png">
+                    <img class="projectImage reveal" src="/assets/ressources/projets/kamaseisme_map.png">
+                </div>
+            </div>
+        </div>
+
+        <div class="mainDiv">
+            <div class="backgroundDiv projectDiv">
                 <label class="projectTitle"><u>Escape the Hack</u></label>
+                <label class="projectDate">2023</label>
                 <div class="projectContentDiv">
                     <div class="projectTextDiv">
                         <label class="projectSubtitle">Présentation</label>
@@ -113,6 +152,7 @@ class Projets
         <div class="mainDiv">
             <div class="backgroundDiv projectDiv">
                 <label class="projectTitle"><u>Ovni Clicker</u></label>
+                <label class="projectDate">2023</label>
                 <div class="projectContentDiv">
                     <div class="projectTextDiv">
                         <label class="projectSubtitle">Présentation</label>
@@ -142,6 +182,7 @@ class Projets
         <div class="mainDiv">
             <div class="backgroundDiv projectDiv">
                 <label class="projectTitle"><u>Pokédex</u></label>
+                <label class="projectDate">2022</label>
                 <div class="projectContentDiv">
                     <div class="projectTextDiv">
                         <label class="projectSubtitle">Présentation</label>
